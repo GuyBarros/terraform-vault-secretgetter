fe862d570be8e82e407cdece6967886be90bcbc9
Guy Barros <38210175+GuyBarros@users.noreply.github.com>
2018-09-19 20:15:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
-provider "vault" {
- version = "~> 1.1"
-}
 module "vault-secretgetter"{
-    source = "./vault-secretgetter"
-    token = "${var.token}"
-secretpath = "${var.secretpath}" 
-vaulthost = "${var.vaulthost}"
-  version = "v.0.0.2"
+ source = "./vault-secretgetter"
+ token = "${var.token}"
+ secretpath = "${var.secretpath}" 
+ vaulthost = "${var.vaulthost}"
+ version = "v.0.0.2"
 }
 
 
