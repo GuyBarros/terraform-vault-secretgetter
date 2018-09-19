add43faa0b0fe7384bff5799fbcc350eb78e9899
Guy Barros <38210175+GuyBarros@users.noreply.github.com>
2018-09-19 16:33:18 +0100

    rename output
    and try to read it from local exec via module

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ vaulthost = "${var.vaulthost}"
 resource "null_resource" "cluster" {
   # Changes to any instance of the cluster requires re-provisioning
        provisioner "local-exec" {
-    command = "echo ${module.vault-secretgetter.mysecret}",
+    command = "echo ${data.terraform_remote_state.emea_se_playground_vault_secret_getter.mysecret}",
   }
 
 }
 
 
 
-output "test-sebastien" {
+output "mysecret" {
  value = "${module.vault-secretgetter.mysecret}"
 }
