aa24b56ed4690c45de076b72a2b8a295a878425f
Guy Barros <38210175+GuyBarros@users.noreply.github.com>
2018-09-19 20:05:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "vault-secretgetter"{
     token = "${var.token}"
 secretpath = "${var.secretpath}" 
 vaulthost = "${var.vaulthost}"
-    version = "0.0.1"
+  version = "v.0.0.2"
 }
 
 
@@ -11,9 +11,6 @@ resource "null_resource" "cluster" {
   # Changes to any instance of the cluster requires re-provisioning
        provisioner "local-exec" {
   command = "echo ${data.terraform_remote_state.emea_se_playground_vault_secret_getter.mysecret}",
-  command = "echo hi mom",
-  
-           
            }
 
 }
